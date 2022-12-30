32ce83071cd6af739bdd0c9732c32671f3567935
tangrc99 <trc99@qq.com>
2022-12-31 03:31:01 +0800

    add trieTree impl

---
+++ db/structure/trie_tree.go
@@ -0,0 +1,172 @@
+package structure
+
+type TrieTreeNode struct {
+	Key      string                   // 键
+	Value    any                      // 值
+	isLeaf   bool                     // 判别是否为叶子节点
+	parent   *TrieTreeNode            // 父结点
+	children map[string]*TrieTreeNode // 子节点链表
+	tree     *TrieTree                // 所属的树
+}
+
+func newTrieTreeNode(key string, value any, leaf bool, parent *TrieTreeNode, owner *TrieTree) *TrieTreeNode {
+	return &TrieTreeNode{
+		Key:      key,
+		Value:    value,
+		isLeaf:   leaf,
+		parent:   parent,
+		children: make(map[string]*TrieTreeNode),
+		tree:     owner,
+	}
+}
+
+type TrieTree struct {
+	root  *TrieTreeNode // 根节点
+	count int           // 叶节点数量
+}
+
+func NewTrieTree() *TrieTree {
+	tree := TrieTree{
+		root: newTrieTreeNode("", nil, false, nil, nil),
+	}
+	tree.root.tree = &tree
+	return &tree
+}
+
+func (tree *TrieTree) AddNode(paths []string, key string, value any) {
+	cur := tree.root
+
+	for _, path := range paths {
+		node, exists := cur.children[path]
+		if !exists {
+			node = newTrieTreeNode(path, nil, false, cur, tree)
+			cur.children[path] = node
+		}
+		cur = node
+	}
+	cur.children[key] = newTrieTreeNode(key, value, true, cur, tree)
+	tree.count++
+}
+
+func (tree *TrieTree) DeleteLeafNode(node *TrieTreeNode) bool {
+	if node == nil || !node.isLeaf || node.tree != tree {
+		return false
+	}
+
+	cur := node.parent
+	delete(cur.children, node.Key)
+
+	for cur != nil && !cur.isLeaf && len(cur.children) == 0 {
+		nxt := cur.parent
+		if nxt != nil {
+			delete(nxt.children, cur.Key)
+		}
+		cur = nxt
+	}
+	tree.count--
+	return true
+}
+
+func (tree *TrieTree) DeletePath(paths []string) bool {
+	cur := tree.root
+
+	// 遍历到最底下的节点
+	for _, path := range paths {
+		node, exists := cur.children[path]
+		if !exists {
+			return false
+		}
+		cur = node
+	}
+
+	return tree.DeleteLeafNode(cur)
+}
+
+func (tree *TrieTree) IsPathExist(paths []string) bool {
+
+	cur := tree.root
+
+	for _, path := range paths {
+		node, exists := cur.children[path]
+		if !exists {
+			return false
+		}
+		cur = node
+	}
+	return cur.isLeaf
+}
+
+func (tree *TrieTree) GetValue(paths []string) (any, bool) {
+	cur := tree.root
+
+	for _, path := range paths {
+		node, exists := cur.children[path]
+		if !exists {
+			return nil, false
+		}
+		cur = node
+	}
+	return cur.Value, cur.isLeaf
+}
+
+// AllLeafNodeInPath 返回当前路径以及路径下的叶子节点（非递归）
+func (tree *TrieTree) AllLeafNodeInPath(paths []string) []*TrieTreeNode {
+	cur := tree.root
+
+	for _, path := range paths {
+		node, exists := cur.children[path]
+		if !exists {
+			return nil
+		}
+		cur = node
+	}
+	r := make([]*TrieTreeNode, 0)
+
+	// 判断当前节点
+	if cur.isLeaf {
+		r = append(r, cur)
+	}
+
+	// 判断子节点
+	for _, node := range cur.children {
+		if node.isLeaf {
+			r = append(r, node)
+		}
+	}
+	return r
+}
+
+// dfsGetLeafNodes 返回当前路径以及路径下的叶子节点（递归）
+func (tree *TrieTree) dfsGetLeafNodes(node *TrieTreeNode, r *[]*TrieTreeNode) {
+
+	if node == nil || node.tree != tree {
+		return
+	}
+
+	if node.isLeaf {
+		*r = append(*r, node)
+	}
+
+	for _, child := range node.children {
+
+		tree.dfsGetLeafNodes(child, r)
+	}
+}
+
+// AllLeafNodeInPathRecursive 返回当前路径以及路径下的叶子节点（递归）
+func (tree *TrieTree) AllLeafNodeInPathRecursive(paths []string) []*TrieTreeNode {
+	cur := tree.root
+
+	for _, path := range paths {
+		node, exists := cur.children[path]
+		if !exists {
+			return nil
+		}
+		cur = node
+	}
+	r := make([]*TrieTreeNode, 0)
+
+	tree.dfsGetLeafNodes(cur, &r)
+
+	return r
+}
--- main.go
+++ main.go
@@ -2,22 +2,40 @@ package main
 
 import (
 	"MemTable/config"
+	"MemTable/db/structure"
 	"MemTable/server"
 	"fmt"
-	"path"
-	"strings"
 )
 
-func main() {
-	p, f := path.Split("/hjg/fggf/gf")
-	segs := strings.Split(p, "/")
-	println(p)
-	println(f)
-	for _, seg := range segs {
-		println(seg)
+func TrieTreeTest(s *[]string) {
+	tree := structure.NewTrieTree()
+
+	path1 := []string{"a", "b", "c"}
+	path2 := []string{"a", "b", "e"}
+
+	tree.AddNode(path1, "d", "path1")
+	tree.AddNode(path2, "f", "path2")
+	path3 := []string{"a", "b", "c", "d"}
+	path4 := []string{"a", "b", "e", "f"}
+
+	path0 := []string{"b"}
+
+	nodes := tree.AllLeafNodeInPath(path0)
+	for _, node := range nodes {
+		println(node.Value.(string))
 	}
 
-	return
+	println(tree.IsPathExist(path1))
+	println(tree.IsPathExist(path3))
+	println(tree.IsPathExist(path4))
+
+	println(tree.DeletePath(path3))
+	println(tree.IsPathExist(path3))
+	println(tree.DeletePath(path4))
+	println(tree.IsPathExist(path4))
+}
+
+func main() {
 
 	s := server.NewServer(fmt.Sprintf("%s:%d", config.Conf.Host, config.Conf.Port))
 	s.Start()
