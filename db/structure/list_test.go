60b36dbccdd971aa448592fd72f9af63652627d1
tangrc99 <trc99@qq.com>
2023-02-27 14:59:35 +0800

    add eviction interface and lru,lfu TODO: add cost of each structure

--- db/structure/list_test.go
+++ db/structure/list_test.go
@@ -1,36 +1,38 @@
 package structure
 
-//func TestList(t *testing.T) {
-//	list := NewList()
-//	list.PushBack(0)
-//	list.PushBack(1)
-//	list.PushBack(2)
-//	list.PushBack(3)
-//	if list.Set(645, 4) || list.Set(645, -100) {
-//		t.Error("Set Failed")
-//	}
-//
-//	if !list.Set(645, -1) && list.Back().(int) != 645 {
-//		t.Error("Set Failed")
-//	}
-//
-//	if list.Size() != 4 {
-//		t.Error("Size Failed")
-//	}
-//
-//	if _, n := list.Range(-1, -1); n != 1 {
-//		t.Error("Range Failed")
-//	}
-//	if _, n := list.Range(0, -1); n != 4 {
-//		t.Error("Range Failed")
-//	}
-//	if _, n := list.Range(-100, -1); n != 4 {
-//		t.Error("Range Failed")
-//	}
-//	if v, ok := list.Pos(-1); !ok || v.(int) != 645 {
-//		t.Error("Pos Failed")
-//	}
-//	if _, ok := list.Pos(100); ok {
-//		t.Error("Pos Failed")
-//	}
-//}
+import "testing"
+
+func TestList(t *testing.T) {
+	list := NewList()
+	list.PushBack(0)
+	list.PushBack(1)
+	list.PushBack(2)
+	list.PushBack(3)
+	if list.Set(645, 4) || list.Set(645, -100) {
+		t.Error("Set Failed")
+	}
+
+	if !list.Set(645, -1) && list.Back().(int) != 645 {
+		t.Error("Set Failed")
+	}
+
+	if list.Size() != 4 {
+		t.Error("Size Failed")
+	}
+
+	if _, n := list.Range(-1, -1); n != 1 {
+		t.Error("Range Failed")
+	}
+	if _, n := list.Range(0, -1); n != 4 {
+		t.Error("Range Failed")
+	}
+	if _, n := list.Range(-100, -1); n != 4 {
+		t.Error("Range Failed")
+	}
+	if v, ok := list.Pos(-1); !ok || v.(int) != 645 {
+		t.Error("Pos Failed")
+	}
+	if _, ok := list.Pos(100); ok {
+		t.Error("Pos Failed")
+	}
+}
