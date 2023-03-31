8ed5fd92e9fbcbbb4a8b69909e4236630fc7ae8e
tangrc99 <trc99@qq.com>
2023-03-31 20:22:46 +0800

    Add some log interface

--- logger/logger.go
+++ logger/logger.go
@@ -113,6 +113,17 @@ func Debug(v ...any) {
 	logger.Println(v)
 }
 
+// Debugf 写入 DEBUG 等级日志
+func Debugf(format string, v ...any) {
+	if logcfg.Level > DEBUG {
+		return
+	}
+	logMu.Lock()
+	defer logMu.Unlock()
+	setPrefix(DEBUG)
+	logger.Printf(format, v)
+}
+
 // Info 写入 INFO 等级日志
 func Info(v ...any) {
 	if logcfg.Level > INFO {
@@ -122,6 +133,17 @@ func Info(v ...any) {
 	defer logMu.Unlock()
 	setPrefix(INFO)
 	logger.Println(v)
+}
+
+// Infof 写入 INFO 等级日志
+func Infof(format string, v ...any) {
+	if logcfg.Level > INFO {
+		return
+	}
+	logMu.Lock()
+	defer logMu.Unlock()
+	setPrefix(INFO)
+	logger.Printf(format, v)
 }
 
 // Warning 写入 WARNING 等级日志
@@ -135,6 +157,17 @@ func Warning(v ...any) {
 	logger.Println(v)
 }
 
+// Warningf 写入 WARNING 等级日志
+func Warningf(format string, v ...any) {
+	if logcfg.Level > WARNING {
+		return
+	}
+	logMu.Lock()
+	defer logMu.Unlock()
+	setPrefix(WARNING)
+	logger.Printf(format, v)
+}
+
 // Error 写入 ERROR 等级日志
 func Error(v ...any) {
 	if logcfg.Level > ERROR {
@@ -146,6 +179,17 @@ func Error(v ...any) {
 	logger.Println(v)
 }
 
+// Errorf 写入 ERROR 等级日志
+func Errorf(format string, v ...any) {
+	if logcfg.Level > ERROR {
+		return
+	}
+	logMu.Lock()
+	defer logMu.Unlock()
+	setPrefix(ERROR)
+	logger.Printf(format, v)
+}
+
 // Panic 写入 PANIC 等级日志
 func Panic(v ...any) {
 	if logcfg.Level > PANIC {
@@ -155,4 +199,15 @@ func Panic(v ...any) {
 	defer logMu.Unlock()
 	setPrefix(PANIC)
 	logger.Fatal(v)
+}
+
+// Panicf 写入 PANIC 等级日志
+func Panicf(format string, v ...any) {
+	if logcfg.Level > PANIC {
+		return
+	}
+	logMu.Lock()
+	defer logMu.Unlock()
+	setPrefix(PANIC)
+	logger.Fatalf(format, v)
 }
