aae0c4a77f15d2f7796781d7e82b8fdff3b6775a
tangrc99 <trc99@qq.com>
2023-04-03 11:09:16 +0800

    The cost calculation is realized

--- db/structure/empty.go
+++
@@ -1,7 +0,0 @@
-package structure
-
-type Empty struct{}
-
-func (e Empty) Cost() int64 {
-	return 0
-}
--- db/structure/structure.go
+++ db/structure/structure.go
@@ -1,6 +1,8 @@
 package structure
 
-type CostCounter interface {
+// Object 是数据库中存储的基本类型
+type Object interface {
+	// Cost 会返回当前元素所占用的内存大小
 	Cost() int64
 }
 
@@ -12,4 +14,31 @@ func (Int64) Cost() int64 {
 
 func (i Int64) Value() int64 {
 	return int64(i)
+}
+
+type Nil struct{}
+
+func (e Nil) Cost() int64 {
+	return 0
+}
+
+type Float32 float32
+
+func (Float32) Cost() int64 {
+	return 4
+}
+func (i Float32) Value() float32 {
+	return float32(i)
+}
+
+type Slice []byte
+
+func (s Slice) Cost() int64 {
+	return int64(len(s))
+}
+
+type String string
+
+func (s String) Cost() int64 {
+	return int64(len(s))
 }
